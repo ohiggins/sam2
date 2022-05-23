462659f14d512133573245177fcc86a7389a4c24
Oliver Higgins <oliver.higgins@gmail.com>
2022-05-23 14:09:45 +1000

    update to color var

--- OpenCV/ball_tracking.py
+++ OpenCV/ball_tracking.py
@@ -86,6 +86,7 @@ while True:
 			cv2.circle(frame, (int(x), int(y)), int(radius),
 				(0, 255, 255), 2)
 			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+print(x)
             
 	# update the points queue
 	pts.appendleft(center)
